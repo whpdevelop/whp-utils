5719aae72c2fde368f344f76f3a83c4e47835d7d
Brian <whpdevelop@sina.com>
2020-09-04 16:12:14 +0800

    package init

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
+<!--
+ * @Author: whp
+ * @Date: 2020-09-04 16:09:36
+ * @LastEditTime: 2020-09-04 16:11:33
+ * @FilePath: /whp-utils/README.md
+-->
 # whp-utils
 common function
+- init
---
+++ index.js
@@ -0,0 +1,17 @@
+/*
+ * @Author: whp
+ * @Date: 2020-09-04 16:10:06
+ * @LastEditTime: 2020-09-04 16:11:10
+ * @FilePath: /whp-utils/index.js
+ */
+
+ export const log = ()=>{
+     console.log(`
+       /*
+        * @Author: whp
+        * @Date: 2020-09-04 16:10:06
+        * @LastEditTime: 2020-09-04 16:10:06
+        * @FilePath: /whp-utils/index.js
+        */
+     `)
+ }
